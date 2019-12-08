b9db82d48909b0f48ada6c230b00a207f20f652f
arbazgormani <arbaz.khan.gormani@gmail.com>
2019-12-09 01:24:57 +0500

    EQuran Tutor BugFix

---
+++ plot.py
@@ -0,0 +1,53 @@
+import glob
+import os
+import librosa
+import librosa.display
+import numpy as np
+import plot
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import specgram
+plt.style.use('ggplot')
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['font.serif'] = 'Ubuntu'
+plt.rcParams['font.monospace'] = 'Ubuntu Mono'
+plt.rcParams['font.size'] = 12
+plt.rcParams['axes.labelsize'] = 11
+plt.rcParams['axes.labelweight'] = 'bold'
+plt.rcParams['axes.titlesize'] = 14
+plt.rcParams['xtick.labelsize'] = 10
+plt.rcParams['ytick.labelsize'] = 10
+plt.rcParams['legend.fontsize'] = 11
+plt.rcParams['figure.titlesize'] = 13
+def plot_waves(sound_names,raw_sounds):
+    i = 1
+    fig = plt.figure(figsize=(25,60), dpi = 900)
+    for n,f in zip(sound_names,raw_sounds):
+        plt.subplot(10,1,i)
+        librosa.display.waveplot(np.array(f),sr=22050)
+        plt.title(n.title())
+        i += 1
+    plt.suptitle('Figure 1: Waveplot',x=0.5, y=0.915,fontsize=18)
+    plt.show()
+    
+def plot_specgram(sound_names,raw_sounds):
+    i = 1
+    fig = plt.figure(figsize=(25,60), dpi = 900)
+    for n,f in zip(sound_names,raw_sounds):
+        plt.subplot(10,1,i)
+        specgram(np.array(f), Fs=22050)
+        plt.title(n.title())
+        i += 1
+    plt.suptitle('Figure 2: Spectrogram',x=0.5, y=0.915,fontsize=18)
+    plt.show()
+
+def plot_log_power_specgram(sound_names,raw_sounds):
+    i = 1
+    fig = plt.figure(figsize=(25,60), dpi = 900)
+    for n,f in zip(sound_names,raw_sounds):
+        plt.subplot(10,1,i)
+        D = librosa.logamplitude(np.abs(librosa.stft(f))**2, ref_power=np.max)
+        librosa.display.specshow(D,x_axis='time' ,y_axis='log')
+        plt.title(n.title())
+        i += 1
+    plt.suptitle('Figure 3: Log power spectrogram',x=0.5, y=0.915,fontsize=18)
+    plt.show()
